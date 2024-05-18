9b2739e6a72fcb74e72399ff3a28db5e06bebd75
jsericksk <jsericksk@gmail.com>
2024-05-17 22:47:29 -0300

    refactor: Make small changes to ExoPlayerLifecycleController

--- app/src/main/java/com/kproject/simpleplayer/presentation/screens/components/player/ExoPlayerLifecycleController.kt
+++ app/src/main/java/com/kproject/simpleplayer/presentation/screens/components/player/ExoPlayerLifecycleController.kt
@@ -4,6 +4,8 @@ import android.os.Build
 import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -19,7 +21,7 @@ fun ExoPlayerLifecycleController(
     onPauseOrStop: () -> Unit,
     onDispose: () -> Unit
 ) {
-    ActivityLifecycle(
+    LifecycleObserver(
         onStart = {
             if (AndroidSdkVersion > 23) {
                 playerView.onResume()
@@ -49,7 +51,7 @@ fun ExoPlayerLifecycleController(
 }
 
 @Composable
-fun ActivityLifecycle(
+private fun LifecycleObserver(
     lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
     onStart: () -> Unit = {},
     onResume: () -> Unit = {},
@@ -57,23 +59,27 @@ fun ActivityLifecycle(
     onStop: () -> Unit = {},
     onDispose: () -> Unit = {}
 ) {
+    val currentOnStart by rememberUpdatedState(onStart)
+    val currentOnResume by rememberUpdatedState(onResume)
+    val currentOnPause by rememberUpdatedState(onPause)
+    val currentOnStop by rememberUpdatedState(onStop)
+    val currentOnDispose by rememberUpdatedState(onDispose)
+
     DisposableEffect(lifecycleOwner) {
         val observer = LifecycleEventObserver { _, event ->
             when (event) {
-                Lifecycle.Event.ON_START -> onStart.invoke()
-                Lifecycle.Event.ON_RESUME -> onResume.invoke()
-                Lifecycle.Event.ON_PAUSE -> onPause.invoke()
-                Lifecycle.Event.ON_STOP -> onStop.invoke()
-                else -> {
-                    Log.d("ActivityLifecycle", "Called any")
-                }
+                Lifecycle.Event.ON_START -> currentOnStart()
+                Lifecycle.Event.ON_RESUME -> currentOnResume()
+                Lifecycle.Event.ON_PAUSE -> currentOnPause()
+                Lifecycle.Event.ON_STOP -> currentOnStop()
+                else -> Log.d("LifecycleObserver", "Called any")
             }
         }
 
         lifecycleOwner.lifecycle.addObserver(observer)
 
         onDispose {
-            onDispose.invoke()
+            currentOnDispose()
             lifecycleOwner.lifecycle.removeObserver(observer)
         }
     }
